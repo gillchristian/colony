42c6a5a1b601719c9d8c7ba1d44538120ddc7c90
gillchristian <gillchristiang@gmail.com>
2016-07-30 18:50:01 -0300

    Update indentation and code style on sembrador.h

--- src/include/sembrador.h
+++ src/include/sembrador.h
@@ -1,67 +1,83 @@
 #ifndef SEMBRADOR_H
 #define SEMBRADOR_H
 #include <iostream>
-#include "comida.h"
 #include <stdlib.h>
 #include <vector>
+#include "comida.h"
+
 using namespace std;
+
 class sembrador {
+  private:
+    int posx;
+    int posy;
+    int dirx;
+    int diry;
 
-private:
-int posx,posy;
-int dirx,diry;
-
-
-public:
-sembrador ();
-void moverse();
-void sembrar ();
-void posicion(int& x,int& y);
+  public:
+    sembrador ();
+    void moverse();
+    void sembrar ();
+    void posicion(int& x,int& y);
 };
 
+/**
+ * Constructor:
+ *
+ * Initializes the sembrador at a random position ---
+ * on the map and with a random direction -----------
+ */
 sembrador::sembrador() {
-posx=rand()%x_dim_max;
-posy=rand()%y_dim_max;
-dirx=rand()%x_dim_max;
-diry=rand()%y_dim_max;
+  posx = rand() % x_dim_max;
+  posy = rand() % y_dim_max;
+  dirx = rand() % x_dim_max;
+  diry = rand() % y_dim_max;
 }
 
-
+/**
+ * Moves in a random direction on the map
+ */
 void sembrador::moverse () {
+  int moverx;
+  int movery;
 
-int moverx,movery;
+  moverx = dirx - posx;
+  movery = diry - posy;
 
-moverx=dirx-posx;
-movery=diry-posy;
+  if(moverx > 0) {
+    posx++;
+  } else {
+    if (moverx != 0) {
+      posx--;
+    }
+  }
 
-if(moverx>0)
-posx++;
-else
-if (moverx!=0)
-posx--;
-
-if(movery>0)
-posy++;
-else 
-if(movery!=0)
-posy--;
-
-if(posx==dirx && posy==diry) {
-dirx=rand()%x_dim_max;
-diry=rand()%y_dim_max;
+  if(movery > 0) {
+    posy++;
+  } else {
+    if (movery != 0) {
+      posy--;
+    }
+  }
+  if(posx == dirx && posy == diry) {
+    dirx = rand() % x_dim_max;
+    diry = rand() % y_dim_max;
+  }
 }
-}
 
-
+/**
+ *
+ */
 void sembrador::sembrar() {
-COMIDA.aumentar_comida(posx,posy);
+  COMIDA.aumentar_comida(posx,posy);
 }
 
+/**
+ * Setter: x and y positions
+ */
 void sembrador::posicion(int& x, int& y) {
-x=posx;
-y=posy;
-
+  x = posx;
+  y = posy;
 };
-
 
 #endif
