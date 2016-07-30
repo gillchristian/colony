26e66eafa369f1ade92d533b845aec66c98995c9
gillchristian <gillchristiang@gmail.com>
2016-07-30 18:52:01 -0300

    Update indentation and code style on constantes.h

--- src/constantes.h
+++ src/constantes.h
@@ -1,35 +1,27 @@
 #ifndef CONSTANTES_H
 #define CONSTANTES_H
 
+// --- dimensiones ---
+extern const int x_dim_max = 200;
+const int y_dim_max = 200;
 
-//dimensiones
+// --- uc ------------
+const int cant_a_reproducir = 10;
+const int energia_inicial = 50;
+const int energia_max = 150;
+const int cant_ucs = 100;
+const int cant_comer = 15;
 
-extern const int x_dim_max=200;
-const int y_dim_max=200;
+// --- sembradores ---
+const int comida_sembrada = 10;
+const int cant_sembradores = 20;
 
-//uc
+// --- comida --------
+const int max_comida = 100;
+const int invierno = 50;
+const int degradar_invierno = 2;
 
-const int cant_a_reproducir=10;
-const int energia_inicial=50;
-const int energia_max=150;
-const int cant_ucs=100;
-const int cant_comer=15;
-
-//sembradores
-
-const int comida_sembrada=10;
-const int cant_sembradores=20;
-
-//comida
-
-const int max_comida=100;
-const int invierno=50;
-const int degradar_invierno=2;
-
-//cromosoma
-
-const int chance_mutar=20;
-
-
+// --- cromosoma -----
+const int chance_mutar = 20;
 
 #endif
