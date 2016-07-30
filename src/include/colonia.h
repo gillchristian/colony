5d91a0a436873edab3cdd776940c6189db2842f7
gillchristian <gillchristiang@gmail.com>
2016-07-30 18:17:45 -0300

    Update indentation and code style on colonia.h

--- src/include/colonia.h
+++ src/include/colonia.h
@@ -1,118 +1,137 @@
 #ifndef COLONIA_H
 #define COLONIA_H
 #include <iostream>
-#include "sembrador.h"
-#include "constantes.h"
-#include "uc.h"
-#include "comida.h"
 #include <stdlib.h>
 #include <vector>
 #include <fstream>
 #include <map>
+#include "sembrador.h"
+#include "constantes.h"
+#include "uc.h"
+#include "comida.h"
 
 using namespace std;
 
 class colonia{
 
-public:
-colonia ();
-void movimiento();
-uc reproduccion();
-void escribir_datos(ofstream& archi);
-void edades (int edad, uc& UC);
+  public:
+    colonia ();
+    void movimiento();
+    void edades (int edad, uc& UC);
+    void escribir_datos(ofstream& archi);
+    uc reproduccion();
 
-private:
-int tiempo;
-vector <sembrador> SEMBRADOR;
-vector <uc> UC;
-int muertos;
-float promedio_int;
-map <int, cromosoma> edad_ucs;
+  private:
+    int tiempo;
+    int muertos;
+    float promedio_int;
+    vector <sembrador> SEMBRADOR;
+    vector <uc> UC;
+    map <int, cromosoma> edad_ucs;
 } COLONIA;
 
-//el costructor de colonia cumple la funcion de inicializar todos los ucs y sembradores tamb
+/**
+ * Constructor
+ *
+ * The colony constructor also initializes the UCs and Sembradores
+ */
 colonia::colonia () {
+  UC.resize(cant_ucs);
+  for(int i = 0; i < cant_ucs; i++) {
+    uc auxiliar;
+    UC[i]=auxiliar;
+  }
 
-UC.resize(cant_ucs);
-for(int i=0;i<cant_ucs;i++) {
-uc auxiliar;
-UC[i]=auxiliar;}
+  SEMBRADOR.resize(cant_sembradores);
 
-SEMBRADOR.resize(cant_sembradores);
-for(int i=0;i<cant_sembradores;i++) {
-sembrador auxiliar;
-SEMBRADOR[i]=auxiliar;
+  for(int i = 0; i < cant_sembradores; i++) {
+    sembrador auxiliar;
+    SEMBRADOR[i] = auxiliar;
+  }
+
+  tiempo = 0;
 }
 
-tiempo=0;
-}
-
+/**
+ * A tick function, it takes care of all the movements happening in the Colony
+ */
 void colonia::movimiento() {
-muertos=0;
-promedio_int=0;
-tiempo++;
+  muertos = 0;
+  promedio_int = 0;
+  tiempo++;
 
-for(int i=0;i<cant_sembradores;i++) {
-  SEMBRADOR[i].moverse();
-  SEMBRADOR[i].sembrar();
-}
-for(int i=0;i<cant_ucs;i++)
-  UC[i].moverse();
-
-for(int i=0;i<cant_ucs;i++) {
-  if (UC[i].verenergia()==0) {
-    UC[i] = reproduccion();
-    muertos++;
-    edades(UC[i].veredad(), UC[i]);
+  for(int i = 0;i < cant_sembradores; i++) {
+    SEMBRADOR[i].moverse();
+    SEMBRADOR[i].sembrar();
   }
-}
-
-int cont=0;
-for (int i=0;i<cant_ucs;i++) {
-  if (UC[i].verenergia() > 0) {
-    promedio_int += UC[i].calcular_iq() * 1.0;
-    cont++;
+  for(int i=0;i<cant_ucs;i++) {
+    UC[i].moverse();
   }
+
+  for(int i = 0; i < cant_ucs; i++) {
+    if (UC[i].verenergia() == 0) {
+      UC[i] = reproduccion();
+      muertos++;
+      edades(UC[i].veredad(), UC[i]);
+    }
+  }
+
+  int cont = 0;
+  for (int i = 0; i < cant_ucs; i++) {
+    if (UC[i].verenergia() > 0) {
+      promedio_int += UC[i].calcular_iq() * 1.0;
+      cont++;
+    }
+  }
+  promedio_int /= cont;
+
+  if ((tiempo % invierno) == 0)
+   COMIDA.estacion();
 }
-promedio_int /= cont;
 
-if ((tiempo%invierno)==0)
- COMIDA.estacion();
-}
-
-
+/**
+ * Takes care of the reproduction happening in the Colony
+ */
 uc colonia::reproduccion() {
 
-vector <uc> reproduccion_A;
-vector <uc> reproduccion_B;
-uc hijo, madre, padre;
-int mayorA=0,mayorB=0;
+  vector <uc> reproduccion_A;
+  vector <uc> reproduccion_B;
+  uc hijo;
+  uc madre;
+  uc padre;
+  int mayorA = 0;
+  int mayorB = 0;
 
-for(int i=0;i<cant_ucs;i++)
-  if(UC[i].verenergia()!=0) {
-    if(UC[i].calcular_iqA()>mayorA)
-      mayorA=UC[i].calcular_iqA();
-    else if(UC[i].calcular_iqB()>mayorB)
-      mayorB=UC[i].calcular_iqB();
-}
+  for(int i = 0; i < cant_ucs; i++) {
+    if(UC[i].verenergia() != 0) {
+      if(UC[i].calcular_iqA() > mayorA) {
+        mayorA = UC[i].calcular_iqA();
+      } else {
+        if(UC[i].calcular_iqB() > mayorB) {
+          mayorB = UC[i].calcular_iqB();
+        }
+      }
+    }
+  }
 
+  for(int i = 0; i < cant_ucs; i++) {
+    if (UC[i].verenergia() != 0) {
+      if(UC[i].calcular_iqA()>=(mayorA-1)) {
+        reproduccion_A.push_back(UC[i]);
+      } else {
+        if(UC[i].calcular_iqB()>=(mayorB-1)) {
+          reproduccion_B.push_back(UC[i]);
+        }
+      }
+    }
+  }
 
-for(int i=0;i<cant_ucs;i++)
-if (UC[i].verenergia()!=0) {
-  if(UC[i].calcular_iqA()>=(mayorA-1))
-    reproduccion_A.push_back(UC[i]);
-  else if(UC[i].calcular_iqB()>=(mayorB-1))
-    reproduccion_B.push_back(UC[i]);}
-
-
-int a=rand()%reproduccion_A.size();
-madre=reproduccion_A[a];
-int b=rand()%reproduccion_B.size();
-padre=reproduccion_B[b];
-
-hijo=padre.cruzar(madre);
-
-return hijo;
+  int a = rand() % reproduccion_A.size();
+  madre = reproduccion_A[a];
+  int b = rand() % reproduccion_B.size();
+  padre = reproduccion_B[b];
+  hijo = padre.cruzar(madre);
+  return hijo;
 }
 
 void colonia::escribir_datos(ofstream& archi) {
@@ -120,7 +139,6 @@ void colonia::escribir_datos(ofstream& archi) {
     "Cantidad de comida en el mapa: " << COMIDA.total() << ", " <<
     "UC que murieron: " << muertos << ", " <<
     "Promedio de inteligencia: " << promedio_int << endl;
-  // map <int>
 }
 
 void colonia::edades (int edad, uc& UC) {
